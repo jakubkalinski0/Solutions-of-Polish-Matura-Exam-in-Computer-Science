34eebf3056ee1f18aedd8dc435819c49aa89f5f0
jakubkalinski0 <kubakalinski2@gmail.com>
2026-06-06 13:53:41 +0200

    feat(coding-tasks): add mr_sf_2012_szyfr

---
+++ coding_tasks/mr_sf_2012_szyfr/a.py
@@ -0,0 +1,25 @@
+plik1=open("tj.txt","r")
+lista1=plik1.readlines()
+plik2=open("klucze1.txt","r")
+lista2=plik2.readlines()
+odp=open("wynik4a.txt","w")
+alfabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def szyfr(slowo1,slowo2):
+    znak=""
+    napis=""
+    if len(slowo1)<=len(slowo2):
+        for i in range(len(slowo1)):
+            znak=chr((ord(slowo1[i])-65+(alfabet.index(slowo2[i])+1))%26+65)
+            napis+=znak
+    else:
+        for i in range(len(slowo1)):
+            znak=chr((ord(slowo1[i])-65+(alfabet.index(slowo2[i%len(slowo2)])+1))%26+65)
+            napis+=znak
+    return napis
+for i in range(len(lista1)):
+    lista1[i]=lista1[i].strip()
+    lista2[i]=lista2[i].strip()
+    zaszyfrowane=szyfr(lista1[i],lista2[i])
+    print(zaszyfrowane)
+    odp.write(zaszyfrowane)
+    odp.write("\n")
